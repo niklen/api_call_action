6fbc7126b997afe15f4969a5227cc1a9cde4fcd1
Niklas Lennerdahl <niklas.lennerdahl@omegapoint.se>
2023-09-27 13:11:19 +0200

    update index

--- dist/index.js
+++ dist/index.js
@@ -9463,34 +9463,36 @@ function parseJwt(token) {
 try {
     const tenantId = core.getInput('tenant_id', { required: true });
     const clientId = core.getInput('client_id', { required: true });
+    const scope = core.getInput('scope', { required: true })
     const tokenEndpoint = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`
 
     core.getIDToken().then(token => {
         core.debug('Acquired Github Action Token')
-        core.debug(parseJwt(token));
+        core.debug(token);
 
         axios.post(tokenEndpoint, {
             client_id: clientId,
             grant_type: 'client_credentials',
             client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-            client_assertion: token
+            client_assertion: token,
+            scope: scope
         }, {
             headers: { 'content-type': 'application/x-www-format-urlencoded' }
         }).then(resp => {
             core.info('Sucessfully acquired Microsoft Entra Id Access Token ')
             core.setOutput('access_token', resp.data['access_token']);
             core.debug(parseJwt(response.data['access_token']))
-        }).catch(err => {
-            core.setFailed(`Failed to acquire Microsoft Entra Id Access token: ${err.message}`);
-            core.error(err.message);
-
+        }).catch(error => {
+            core.error(error.message)
+            core.debug('Test')
+            core.setFailed(`Failed to Acquire Azure AD JWT: ${error.message}`)
         })
     }).catch(err => {
         core.setFailed(`Failed to acquire GitHub Action Token: ${err.message}`);
         core.error(err.message);
     })
 } catch (error) {
-
+    core.setFailed(error.message);
 }
 })();
 
